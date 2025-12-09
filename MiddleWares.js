4042184f9ee724798233313b70d4a5ab438fa218
KSriMounika <23P31A4235@acet.ac.in>
2025-12-09 10:37:31 +0530

    Ensure MiddleWares.js is tracked (mixed-case filename)

--- MiddleWares.js
+++ MiddleWares.js
@@ -5,9 +5,14 @@ const app = express();
 // app.use(express.bodyParser())
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    console.log("This is from backend");
+    res.send("Home");
+})
+
 app.get('/get-user', (req,res) => {
 
-    res.send("api success");
+    res.send("api successful");
 })
 
 app.post('/add-user', (req, res) => {
